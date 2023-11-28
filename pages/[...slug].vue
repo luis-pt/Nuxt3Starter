abd5cb2dc117f13855ed2d8b9834c798cd7bf400
Luís Carvalho <mail@luis.pt>
2023-11-28 20:26:41 +0000

    troca de index para slug

---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<script setup>
+    const { data, pending, error, refresh } = await useFetch('http://teste.com.pt/bliss-market/wp-json/wp/v2/posts', {
+        query: { slug: 'ux-pie' }
+    })
+    const route = useRoute()
+</script>
+<template>
+    <div>
+        <NavBar />
+        <br>
+        <hr>
+        <br>
+        <!-- {{ data }} -->
+        {{ route }}
+        <br><br>
+        <hr>
+        <br>
+        <CarHero />
+    </div>
+</template>
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-
-<template>
-    <div>
-        <NavBar />
-        <CarHero />
-    </div>
-</template>
